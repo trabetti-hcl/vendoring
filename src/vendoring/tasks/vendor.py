7fcd93c3fd57656f2b5545eeb668e84cf814f592
Tali <61489860+trabetti-hcl@users.noreply.github.com>
2026-08-27 08:31:42 +0300

    Update vendor.py

--- src/vendoring/tasks/vendor.py
+++ src/vendoring/tasks/vendor.py
@@ -27,6 +27,7 @@ def download_libraries(requirements: Path, destination: Path) -> None:
         # We use --no-deps because we want to ensure that dependencies are provided.
         # This includes all dependencies recursively up the chain.
         "--no-deps",
+        "--only-binary :all:"
     ]
     run(command, working_directory=None)
 
