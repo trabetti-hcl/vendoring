1e2a0ef1f8b6db8036837bcad5fd2c8dbd672098
Tali <61489860+trabetti-hcl@users.noreply.github.com>
2026-08-27 09:05:50 +0300

    Update vendor.py

--- src/vendoring/tasks/vendor.py
+++ src/vendoring/tasks/vendor.py
@@ -18,6 +18,7 @@ def download_libraries(requirements: Path, destination: Path) -> None:
         "pip",
         "install",
         "--only-binary=:all:",
+        "--uploaded-prior-to=P7D",
         "--platform",
         "any",
         "-t",
